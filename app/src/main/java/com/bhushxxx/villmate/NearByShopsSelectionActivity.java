5f728bf917cbccf8d2402e04e0af9fcc576173aa
bikkibhushan1 <bikkibhushan1@gmail.com>
2019-08-06 22:13:59 +0530

    Initial commit

--- app/src/main/java/com/bhushxxx/villmate/NearByShopsSelectionActivity.java
+++ app/src/main/java/com/bhushxxx/villmate/NearByShopsSelectionActivity.java
@@ -3,7 +3,9 @@ package com.bhushxxx.villmate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class NearByShopsSelectionActivity extends AppCompatActivity {
@@ -13,15 +15,38 @@ public class NearByShopsSelectionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_near_by_shops_selection);
 
+        TextView cat_sel_text = findViewById(R.id.cat_sel_text);
+        ImageView cat_sel_image = findViewById(R.id.cat_sel_image);
+
 
 
         Intent intent = getIntent();
-
         int cat_sel= intent.getIntExtra("cat_sel", 1);
 
-        TextView cat_sel_text = findViewById(R.id.cat_sel_text);
-
         cat_sel_text.setText(cat_sel + " selected");
+
+        switch (cat_sel){
+
+            case 1 : cat_sel_image.setBackgroundResource(R.drawable.fertilizers);
+            break;
+            case 2 : cat_sel_image.setBackgroundResource(R.drawable.seeds);
+            break;
+            case 3 : cat_sel_image.setBackgroundResource(R.drawable.building_materials);
+            break;
+            case 4 : cat_sel_image.setBackgroundResource(R.drawable.pesticides);
+
+
+
+
+
+        }
+
+
+
+
+
+
+
 
 
 
